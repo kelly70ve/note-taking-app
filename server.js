f286299d9f2122014692868cd3f5029c743f1b1a
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-06-20 00:32:08 -0700

    tried creating an alert for duplicate title

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.post("/api/notes", function (req, res) {
 
     var notes = JSON.parse(data);
 
+    // notes.map(note => {
+    //   if (note.id === newNote.id) {
+    //     return alert("Note title has already been filed, please enter a new note title")
+    //   }
+    // })
+
     var addedNote = [...notes, newNote];
 
     fs.writeFile("db/db.json", JSON.stringify(addedNote), (err, data) => {
@@ -65,9 +71,6 @@ app.post("/api/notes", function (req, res) {
 
   res.json(newNote);
 }); 
-
-
-// ABOVE WORKS ***** 
 
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
@@ -91,7 +94,6 @@ app.delete("/api/notes/:id", function (req, res) {
       console.log("A note has been deleted");
     })
   });
-  
   res.json(deleteNote)
 }); 
 
